f0cac895a019b763c9f710aaa127950e9615d305
curvaboy <lin88zhang@163.com>
2020-11-24 18:04:35 +0800

    wwen

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(400, 200)
+    w.move(300, 300)
+    w.setWindowTitle('first')
+    w.show()
+    app.exit(app.exec_())
